4ac5ca60932f1b87a8d981db6dee2d66590f56e0
Grant DeLong <gdelong1@geisinger.edu>
2023-06-06 14:21:24 -0500

    added Size component

--- sentency/sentency.py
+++ sentency/sentency.py
@@ -127,3 +127,112 @@ class Sentex:
         start = span.start + offset
         end = span.end + offset
         return doc[start:end]
+    
+@Language.factory(
+    "size",
+    default_config={
+        "size_regex": r"(i?)((\d+(\.\d+)?)\s*([Xx]\s*(\d+(\.\d+)?)\s*([Xx]\s*(\d+(\.\d+)?))?)?\s*)(?=\W?[cm]m)",
+        "sentex_only": True,
+        "annotate_ents": True,
+        "label": "SIZE"
+    },
+)
+def create_size_component(
+    nlp: Language,
+    name: str,
+    size_regex: str,
+    sentex_only: bool,
+    annotate_ents: bool,
+    label: str
+):
+    return Size(nlp, size_regex, sentex_only, annotate_ents, label)
+
+
+class Size:
+    """
+    Size is a spaCy pipeline component that adds spans to the list `Doc._.size`
+    based on regular expression matches within each sentence of the document. 
+
+    nlp: `Language`,
+        A required argument for spacy to use this as a factory
+    size_regex : `str`,
+        A regular expression to match spans within each sentence of the document.
+    sentex_only : `bool`,
+        Only match in sentences with Sentex-matched entities
+    annotate_ents: `bool`,
+        Write/overwrite matches to Doc.ents
+    label: `str`,
+        If annotate_ents == True, the label for the matched entity
+    """
+
+    def __init__(
+        self,
+        nlp: Language,
+        size_regex: str,
+        sentex_only: bool,
+        annotate_ents: bool,
+        label: str
+    ):
+        self.size_regex = size_regex
+        self.sentex_only = sentex_only
+        self.annotate_ents = annotate_ents
+        self.label = label
+
+        if not Doc.has_extension("size"):
+            Doc.set_extension("size", default=[])
+
+    def __call__(self, doc: Doc) -> Doc:
+        self.process(doc)
+        if self.annotate_ents:
+            self.set_annotations(doc)
+        return doc
+    
+    def set_annotations(self, doc):
+        """Modify the document in place.
+        Logic taken from spacy.pipeline.entityruler.EntityRuler
+        """
+        entities = list(doc.ents)
+        logger.debug(f"current entities: {entities}")
+        new_entities = []
+        seen_tokens = set()
+        matches = self._get_matches(doc)
+        for match_id, start, end in matches:
+            # check for end - 1 here because boundaries are inclusive
+            if start not in seen_tokens and end - 1 not in seen_tokens:
+                span = Span(doc, start, end, label=match_id)
+                new_entities.append(span)
+                logger.debug(f"new entity: {span.text} start: {start} end: {end}")
+                entities = [
+                    e for e in entities if not (e.start < end and e.end > start)
+                ]
+
+                seen_tokens.update(range(start, end))
+        doc.ents = entities + new_entities
+    
+    def process(self, doc: Doc) -> Doc:
+        if self.sentex_only:
+            sents = set([span.sent for span in doc._.sentex])
+        else:
+            sents = doc.sents
+        for sent in sents:
+            match = re.search(self.size_regex, str(sent))
+            if match is None:
+                continue
+            start, end = match.span()
+            num_start = sent.start_char + start
+            num_end = sent.start_char + end
+
+            span = sent.doc.char_span(num_start,num_end, alignment_mode='contract')
+            logger.debug(f'span: {span} indexes: {num_start} to {num_end}')
+            size = self.parse_size(str(span))
+            doc._.size.append((span, size))
+
+    def _get_matches(self, doc: Doc):
+        return [(self.label, m[0].start, m[0].end) for m in doc._.size]
+    
+    def parse_size(self, text: str) -> float:
+        sizes = [n for n in text.lower().replace('x',' ').split(' ') if n != '']
+        sizes = [float(n) for n in sizes]
+        sizes = sizes if len(sizes) <= 2 else sizes[:2]
+        size = max(sizes)
+        return size
--- tests/test_sentex.py
+++ tests/test_sentex.py
@@ -66,8 +66,93 @@ def test_sentex_annotate_ents_expand_mode():
             "label": "NEG_DESC",
         },
     )
+    
 
     doc = nlp(text)
 
     assert doc.ents[0].text == "aggressive"
     assert doc.ents[1].text == "aggressive"
+
+def test_size_single_number():
+    text = """
+    Screening for abdominal aortic aneurysm.
+    Impression: There is evidence of a fusiform
+    abdominal aortic aneurysm measuring 3.4 cm.
+    """
+    keyword_regex = regexize_keywords(aaa_keywords)
+    ignore_regex = regexize_keywords(ignore_keywords)
+
+    nlp = spacy.load("en_core_web_sm")
+    nlp.add_pipe(
+        "sentex",
+        config={
+            "sentence_regex": keyword_regex,
+            "ignore_regex": ignore_regex,
+            "annotate_ents": True,
+            "label": "AAA",
+        },
+    )
+    nlp.add_pipe(
+        "size"
+        )
+
+    doc = nlp(text)
+
+    assert doc._.size[0][1] == 3.4
+    assert doc.ents[1].text == "3.4"
+
+def test_size_two_numbers():
+    text = """
+    Screening for abdominal aortic aneurysm.
+    Impression: There is evidence of a fusiform
+    abdominal aortic aneurysm measuring 3.4 x 3.5 cm.
+    """
+    keyword_regex = regexize_keywords(aaa_keywords)
+    ignore_regex = regexize_keywords(ignore_keywords)
+
+    nlp = spacy.load("en_core_web_sm")
+    nlp.add_pipe(
+        "sentex",
+        config={
+            "sentence_regex": keyword_regex,
+            "ignore_regex": ignore_regex,
+            "annotate_ents": True,
+            "label": "AAA",
+        },
+    )
+    nlp.add_pipe(
+        "size"
+        )
+
+    doc = nlp(text)
+
+    assert doc._.size[0][1] == 3.5
+    assert doc.ents[1].text == "3.4 x 3.5"
+
+def test_size_two_numbers():
+    text = """
+    Screening for abdominal aortic aneurysm.
+    Impression: There is evidence of a fusiform
+    abdominal aortic aneurysm measuring 3.4 x 3.5 x 3.6 cm.
+    """
+    keyword_regex = regexize_keywords(aaa_keywords)
+    ignore_regex = regexize_keywords(ignore_keywords)
+
+    nlp = spacy.load("en_core_web_sm")
+    nlp.add_pipe(
+        "sentex",
+        config={
+            "sentence_regex": keyword_regex,
+            "ignore_regex": ignore_regex,
+            "annotate_ents": True,
+            "label": "AAA",
+        },
+    )
+    nlp.add_pipe(
+        "size"
+        )
+
+    doc = nlp(text)
+
+    assert doc._.size[0][1] == 3.5
+    assert doc.ents[1].text == "3.4 x 3.5 x 3.6"
